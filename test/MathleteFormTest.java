ffa9759c579f492892eb97c6623081313f2c1da6
kart1618 <kart1618@gmail.com>
2014-03-22 15:37:40 -0500

    Changed names of a few tests. Coverage: 67%

--- test/MathleteFormTest.java
+++ test/MathleteFormTest.java
@@ -9,7 +9,6 @@ public class MathleteFormTest {
 
 	@Test
 	public void testIscharacter_char_pass() {
-		//fail("Not yet implemented");
 		MathleteForm mf=new MathleteForm();
 		char c='c';
 		boolean result = mf.IsCharacter(c);
@@ -18,7 +17,6 @@ public class MathleteFormTest {
 	
 	@Test
 	public void testIscharacter_lowerbound_symbol_fail() {
-		//fail("Not yet implemented");
 		MathleteForm mf=new MathleteForm();
 		char c='#';
 		boolean result = mf.IsCharacter(c);
@@ -57,26 +55,4 @@ public class MathleteFormTest {
 		mf.CalculatorInputTextFieldActionPerformed(new ActionEvent(mf, 2, "5+6"));
 		
 	}
-	/*
-	@Test
-	public void calcTester1()
-	{
-		MathleteForm mf=new MathleteForm();
-		MathleteForm.CalculatorInputTextField.setText("a+6");
-		mf.CalculatorInputTextFieldActionPerformed(new ActionEvent(mf, 2, "a+b"));
-		assertSame("Katie", "Katie");
-		
-	}
-	@Test
-	public void calcTester11()
-	{
-		MathleteForm mf=new MathleteForm();
-		MathleteForm.CalculatorInputTextField.setText("1==1");
-		mf.CalculatorInputTextFieldActionPerformed(new ActionEvent(mf, 2, "a+b"));
-		assertSame("Katie", "Katie");
-		
-	}
-
-
-*/
 }
